40b0d1fb1ba0c86e1af19862c2fbab8840be4800
Nguyen Ngoc Trung <ngoctrunght19@gmail.com>
2019-11-16 22:40:15 +0700

    fix

--- convert_data.py
+++ convert_data.py
@@ -1,5 +1,6 @@
 import json
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 def convert_train():
   with open("data/zalo/train.json", encoding='utf-8') as f:
@@ -11,13 +12,15 @@ def convert_train():
       "label": example["label"]*1,
       "id_1": idx*2,
       "id_2": idx*2 + 1,
-      "text": example["text"].rstrip(),
-      "question": example["question"].rstrip()
+      "text": example["text"].replace("\r", "").replace("\n", ""),
+      "question": example["question"].replace("\r", "").replace("\n", "")
     }
     examples.append(temp)
 
   df = pd.DataFrame(examples)
-  df.to_csv("data/glue/MRPC/train.tsv", sep='\t', encoding='utf-8', index=False)
+  train, test = train_test_split(df, test_size=0.2)
+  train.to_csv("data/glue/MRPC/train.tsv", sep='\t', encoding='utf-8', index=False)
+  test.to_csv("data/glue/MRPC/dev.tsv", sep='\t', encoding='utf-8', index=False)
 
 def convert_test():
   with open("data/zalo/test.json", encoding='utf-8') as f:
@@ -28,7 +31,7 @@ def convert_test():
     temp = example.copy()
     del temp['paragraphs']
     for para in example["paragraphs"]:
-      temp["text"] = para["text"].rstrip()
+      temp["text"] = para["text"].replace("\r", "").replace("\n", "")
       temp["pid"] = para["id"]
       examples.append(temp.copy())
 
--- init.sh
+++ init.sh
@@ -7,3 +7,11 @@ mkdir data/model/output
 
 mkdir data/glue
 mkdir data/glue/MRPC
+
+wget https://dl.challenge.zalo.ai/ZAC2019_VietnameseWikiQA/train.zip
+wget https://dl.challenge.zalo.ai/ZAC2019_VietnameseWikiQA/test.zip
+unzip train.zip data/zalo
+unzip test.zip data/zalo
+
+pip install -r requirements.txt
+python convert_data.py
